8070461a7ae3f2d53721e9a8b28c679579ff19fd
gopal-mahajan <gopal.mahajan@datakaveri.org>
2024-06-21 13:38:49 +0530

    removed-unused-files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/gopal/Documents/datakaveri/ugix-developer-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/gopal/Documents/datakaveri/ugix-developer-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/gopal/Documents/datakaveri/ugix-developer-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/gopal/Documents/datakaveri/ugix-developer-docs/src/css/custom.css"),
-];
